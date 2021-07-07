f4460a5270f45fe30c3ad24f33922e407b20cdfd
AnhTuNguyen <76692449+anhtu031199@users.noreply.github.com>
2021-07-07 15:15:52 +0700

    add feature home, register

--- app/src/main/java/com/example/telesense_day03/MainActivity.kt
+++ app/src/main/java/com/example/telesense_day03/MainActivity.kt
@@ -2,10 +2,24 @@ package com.example.telesense_day03
 
 import androidx.appcompat.app.AppCompatActivity
 import android.os.Bundle
+import android.widget.Button
+import android.widget.TextView
+import androidx.fragment.app.add
+import androidx.fragment.app.commit
 
 class MainActivity : AppCompatActivity() {
+    var btn: TextView? = null
     override fun onCreate(savedInstanceState: Bundle?) {
         super.onCreate(savedInstanceState)
         setContentView(R.layout.activity_main)
+        btn = findViewById(R.id.btn_signup)
+        btn?.setOnClickListener({
+            if (savedInstanceState == null) {
+                supportFragmentManager.commit {
+                    setReorderingAllowed(true)
+                    add<RegisterFragment>(R.id.frame_layout)
+                }
+            }
+        })
     }
 }
