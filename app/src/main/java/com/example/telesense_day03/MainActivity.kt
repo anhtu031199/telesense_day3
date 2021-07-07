c18650cac675f5529d30c6b47d899c35db82eb13
Hung2299 <hungkoj9x@gmail.com>
2021-07-07 15:23:38 +0700

    07/07_ commit1

--- app/src/main/java/com/example/telesense_day03/MainActivity.kt
+++ app/src/main/java/com/example/telesense_day03/MainActivity.kt
@@ -1,11 +1,19 @@
 package com.example.telesense_day03
 
-import androidx.appcompat.app.AppCompatActivity
 import android.os.Bundle
+import androidx.appcompat.app.AppCompatActivity
+import androidx.fragment.app.add
+import androidx.fragment.app.commit
 
 class MainActivity : AppCompatActivity() {
     override fun onCreate(savedInstanceState: Bundle?) {
         super.onCreate(savedInstanceState)
         setContentView(R.layout.activity_main)
+        if (savedInstanceState == null) {
+            supportFragmentManager.commit {
+                setReorderingAllowed(true)
+                add<ForgotPassword>(R.id.fragment)
+            }
+        }
     }
 }
